bd723732b12ab0f7843d4c853d7e0a02165501b3
Alex <alexalonsoportas@gmail.com>
2022-04-11 21:07:27 +0200

    cambios

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def saludo():
-    return 'Aquu estamos....'
-    
+    titulo="Bienvenido"
+    return render_template('index.html', titulo=titulo)
+
 #http://localhost:8118/saludoPersonalizado?nombre=Alex
 @app.route('/saludoPersonalizado')
 def saludoPersonalizado():
     nombre = request.args.get('nombre', 'no pusistes')
     return 'Que pasa {} ??'.format(nombre)
+
+@app.route('/p')
+@app.route('/p/<name>')
+@app.route('/p/<name>/<int:num>')
+def sPersonalizado(name = '', num = ''):
+    if(name==''):
+        return 'No has introducido nombre. Que pasa desconocido ??'
+    else:
+        return 'Que pasa {} ??'.format(name)
+
+@app.route('/user/<name>')
+def user(name = ''):
+    age=132
+    lista=[1,32,True,'Alex']
+    if(name==''):
+        return 'No has introducido nombre. Que pasa desconocido ??'
+    else:
+        return render_template('user.html', nombre = name, age=age, lista=lista)
 
 if __name__ =='__main__':
     app.run(debug  = True, port = 8118)
